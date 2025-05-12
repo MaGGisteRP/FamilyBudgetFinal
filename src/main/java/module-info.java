d7d4b26446569a80139a01ae40a030153f3d99fa
MaGGisteR <susin738@gmail.com>
2025-05-12 17:16:53 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ru.maggister.familybudget {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ru.maggister.familybudget to javafx.fxml;
+    exports ru.maggister.familybudget;
+}
